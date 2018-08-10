8a75b12b2df98aef527a495a64d533c2a37a2692
PapsOu <vivien.frasca@gmail.com>
2018-08-10 14:24:35 +0200

    Add example for bug https://github.com/doctrine/dbal/issues/2880

---
+++ src/Entity/ExampleEntity.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ExampleEntityRepository")
+ */
+class ExampleEntity
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", nullable=true)
+     */
+    private $anArray;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(?string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAnArray()
+    {
+        return $this->anArray;
+    }
+
+    public function setAnArray($anArray): self
+    {
+        $this->anArray = $anArray;
+
+        return $this;
+    }
+}
